e6340892bac7cda503fdbd5554bb042fa22299cc
Bogdan Sanarov <98291482+LemonSkvid@users.noreply.github.com>
2023-12-28 12:32:37 +1000

    Update calculator.py

--- src/01_Calculator/calculator.py
+++ src/01_Calculator/calculator.py
@@ -3,10 +3,8 @@ from typing import List, Optional, Union
 
 ops = {"+": add, "-": sub, "*": mul, "/": truediv}
 
-
 def _split_if_string(string_or_list: Union[List[str], str]) -> List[str]:
     return string_or_list.split() if isinstance(string_or_list, str) else string_or_list
-
 
 def prefix_evaluate(prefix_equation: Union[List[str], str]) -> Optional[int]:
     if not prefix_equation:
@@ -25,18 +23,29 @@ def prefix_evaluate(prefix_equation: Union[List[str], str]) -> Optional[int]:
 
     return value_stack[0]
 
+def to_prefix(equation: str) -> List[str]:
+    precedence = {"+": 1, "-": 1, "*": 2, "/": 2}
+    operators = set("+*-/")
+    output = []
+    stack = []
 
-def to_prefix(equation: str) -> str:
-    op_stack = []
-    prefix = []
+    for token in reversed(equation.split()):
+        if token.isdigit():
+            output.append(token)
+        elif token in operators:
+            while stack and stack[-1] != ")" and precedence[token] <= precedence.get(stack[-1], 0):
+                output.append(stack.pop())
+            stack.append(token)
+        elif token == ")":
+            stack.append(token)
+        elif token == "(":
+            while stack and stack[-1] != ")":
+                output.append(stack.pop())
+            stack.pop()
 
-    for el in equation.split()[::-1]:
-        pass
-
-    while op_stack:
-        prefix.append(op_stack.pop())
-
-    return " ".join(prefix[::-1])
+    while stack:
+        output.append(stack.pop())
+    return list(reversed(output))
 
 
 def calculate(equation: str) -> int:
