39813b47d75935c824f489b9410caac48b9d9944
Bogdan Sanarov <98291482+LemonSkvid@users.noreply.github.com>
2024-01-17 22:02:15 +1000

    Update test_ocean.py

--- src/04_Ocean/test_ocean.py
+++ src/04_Ocean/test_ocean.py
@@ -2,13 +2,15 @@ from typing import ClassVar
 from ocean import Ocean
 
 class Test0cean:
-    init_state: ClassVar[list[list[int]]] = [[0, 2, 0, 1, 3],
+    init_state: ClassVar[list[list[int]]] = [
+        [0, 2, 0, 1, 3],
         [0, 2, 0, 3, 3],
         [0, 2, 2, 2, 1],
         [2, 1, 2, 0, 3],
         [3, 3, 1, 3, 3],
     ]
-    next_state: ClassVar[list[list[int]]] = [[0, 0, 0, 1, 3],
+    next_state: ClassVar[list[list[int]]] = [
+        [0, 0, 0, 1, 3],
         [2, 2, 0, 3, 3],
         [2, 0, 0, 2, 1],
         [0, 1, 2, 2, 3],
